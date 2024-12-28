1ab7a00d57ac5ba7a80c1de2c970f3e1ef43e04d
yash yadav <132824732+yaduwanshiyash@users.noreply.github.com>
2024-12-28 18:33:28 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,8 @@ const CACHE_NAME = 'portfolio-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  'https://images.unsplash.com/photo-1719937051230-8798ae2ebe86?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1719937051230-8798ae2ebe86?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 ];
 
 self.addEventListener('install', event => {
